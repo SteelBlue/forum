938f03ceaea2b1e1a2ef7511ca5b6e79420fd226
Ryan Ebbers <info@ryanebbers.com>
2017-06-02 22:18:50 -0400

    extracted the thread reply to a partial

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -25,14 +25,7 @@
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1">
                             @foreach ($thread->replies as $reply)
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans()  }}...
-                                    </div>
-                                    <div class="panel-body">
-                                        {{ $reply->body  }}
-                                    </div>
-                                </div>
+                                @include ('threads.partials.reply')
                             @endforeach
                         </div>
                     </div>
