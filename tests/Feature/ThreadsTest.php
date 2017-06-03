4f6596fc2ad9adcccbb41e3db7edb56a3bfdbecc
Ryan Ebbers <info@ryanebbers.com>
2017-06-02 21:12:28 -0400

    removed instances of new Thread being created for each test

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -19,8 +19,6 @@ class ThreadsTest extends TestCase
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
 
         $response->assertSee($this->thread->title);
@@ -29,8 +27,6 @@ class ThreadsTest extends TestCase
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads/' . $this->thread->id);
 
         $response->assertSee($this->thread->title);
