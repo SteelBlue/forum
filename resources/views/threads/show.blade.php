16f77ce925d61700fbef7511c85075cbcf5c9c9e
Ryan Ebbers <info@ryanebbers.com>
2017-06-02 22:14:09 -0400

    added link to the reply->owner->name for future linking

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,7 +26,8 @@
                         <div class="col-md-10 col-md-offset-1">
                             @foreach ($thread->replies as $reply)
                                 <div class="panel panel-default">
-                                    <div class="panel-heading">{{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans()  }}</div>
+                                    <div class="panel-heading">
+                                        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans()  }}</div>
                                     <div class="panel-body">
                                         {{ $reply->body  }}
                                     </div>
