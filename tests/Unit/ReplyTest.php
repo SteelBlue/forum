75742cf58c2efa88f5fb04038db9e1a4035dd068
Ryan Ebbers <info@ryanebbers.com>
2017-06-02 22:05:43 -0400

    use DatabaseMigrations for the ReplyTest

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -4,10 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ReplyTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function it_has_an_owner()
     {
