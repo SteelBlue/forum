93919977cef7baf647feb7c074511b0808db7876
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 16:28:24 -0400

    loading files in composer.json for tests

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -15,7 +15,7 @@ class CreateThreadTest extends TestCase
     public function guests_may_not_create_threads()
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
-        
+
         $thread = factory('App\Thread')->make();
 
         $this->post('/threads', $thread->toArray());
