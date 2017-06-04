a323819360f792a94ed00d2dc1a4a7dc1547b83d
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 16:20:30 -0400

    storing the reply in RepliesController, updated test

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -9,13 +9,22 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CreateThreadTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function an_authenticated_user_can_create_a_new_forum_thread()
     {
-        $this->assertTrue(true);
+        // Given we have a signed in user.
+        $this->actingAs(factory('App\User')->create());
+
+        // When we hit the endpoint to create a new thread.
+        $thread = factory('App\Thread')->make();
+        $this->post('/threads', $thread->toArray());
+
+
+        // Then, we visit the new thread page.
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
     }
 }
