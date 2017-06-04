625ac9be32d29df12f4d14691639c4bd1ef0bb73
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 17:20:41 -0400

    forgot to add ThreadsController to threads resource route

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('threads');
+Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
