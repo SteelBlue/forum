c553e8e3886998a8d07a89bdb2ec9e5af6c3813d
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 17:18:55 -0400

    using route resource for threads

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/create', 'ThreadsController@create');
-Route::post('/threads', 'ThreadsController@store');
-Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::resource('threads');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
