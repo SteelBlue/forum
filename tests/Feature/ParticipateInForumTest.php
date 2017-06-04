2cba16572744960abb9ec2f19b5f01b851166cf0
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 00:04:17 -0400

    created test checks unauthenticated users can not reply to a thread

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,6 +10,20 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function unauthenticated_user_may_not_add_reply_in_forum_threads()
+    {
+        // This test expects an Authentication Exception
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        // And an existing thread.
+        $thread = factory('App\Thread')->create();
+
+        // When the user adds a reply to the thread.
+        $reply = factory('App\Reply')->create();
+        $this->post($thread->path() . '/replies', $reply->toArray());
+    }
+
+    /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given an authenticated user.
