84ec4b63cfbfa97ef36481f84ba6e0250a792747
Ryan Ebbers <info@ryanebbers.com>
2017-06-04 17:26:40 -0400

    created view for threads.create, created form for creating thread

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create a New Thread</div>
+
+                    <div class="panel-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" name="title" id="title" class="form-control" placeholder="Title">
+                            </div>
+
+                            <div class="form-group">
+                                <lable for="body">Body</lable>
+                                <textarea id="body" class="form-control" name="body" rows="5"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
