b6c17f67ab856d75217f90f0c5ec55393b69534d
Ryan Ebbers <info@ryanebbers.com>
2017-06-05 18:54:53 -0400

    broke out the way a thread is published to publishThread method with overrides being passed through as arguments

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -45,13 +45,18 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function a_thread_requires_a_title()
     {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    public function publishThread($overrides = [])
+    {
         $this->withExceptionHandling();
 
         $this->signIn();
 
         $thread = make('App\Thread', ['title' => null]);
 
-        $this->post('/threads', $thread->toArray())
-            ->assertSessionHasErrors('title');
+        return $this->post('/threads', $thread->toArray());
     }
 }
