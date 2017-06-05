2858aeccb3390b4a9a051b74f24866b580604227
Ryan Ebbers <info@ryanebbers.com>
2017-06-05 19:10:04 -0400

    created test a_reply_requires_a_body

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -28,10 +28,28 @@ class ParticipateInForumTest extends TestCase
 
         // When the user adds a reply to the thread.
         $reply = make('App\Reply');
+
         $this->post($thread->path() . '/replies', $reply->toArray());
 
         // Then their reply should be visible on the page.
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling();
+
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+
+
     }
 }
