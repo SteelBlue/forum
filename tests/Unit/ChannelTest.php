bc61e9c78e806131cab3fe6ff341e923893ed93f
Ryan Ebbers <info@ryanebbers.com>
2017-06-05 23:16:39 -0400

    creating test a_channel_consists_of_threads

---
+++ tests/Unit/ChannelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ChannelTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function a_channel_consists_of_threads()
+    {
+        
+    }
+}
