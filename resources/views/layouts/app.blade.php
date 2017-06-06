845318ee2f098d81ed78b1d0cb35deec997476cb
Ryan Ebbers <info@ryanebbers.com>
2017-06-05 23:32:12 -0400

    using @foreach query to display all the channels

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,15 +38,13 @@
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
+                                @foreach(App\Channel::all() as $channel)
+                                    <li>
+                                        <a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a>
+                                    </li>
+                                @endforeach
                             </ul>
                         </li>
                     </ul>
