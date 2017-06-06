f970da5543e6f9b413251a8741339d29ea557e1f
Ryan Ebbers <info@ryanebbers.com>
2017-06-05 23:01:29 -0400

    created test for a_user_can_filter_threads_according_to_a_tag

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -24,7 +24,7 @@ class RepliesController extends Controller
         $this->validate(request(), [
             'body' => 'required'
         ]);
-        
+
         $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
