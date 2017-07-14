4c395b4aa8af158f2ccbc91baa4dd5b61ab078f5
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-14 00:37:42 -0400

    pull up the members of the ThreadFilters class to Filters

--- app/Filters/Filters.php
+++ app/Filters/Filters.php
@@ -2,8 +2,39 @@
 
 namespace App\Filters;
 
+use Illuminate\Http\Request;
 
 abstract class Filters
 {
+    /**
+     * @var Request
+     */
+    protected $request;
+    protected $builder;
 
+    /**
+     * ThreadFilters constructor.
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Apply filters to threads query.
+     *
+     * @param $builder
+     * @return mixed
+     */
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        // Check if filter by username.
+        if ($this->request->has('by')) {
+            $this->by($this->request->by);
+        }
+
+        return $this->builder;
+    }
 }
--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -8,38 +8,6 @@ use Illuminate\Http\Request;
 class ThreadFilters extends Filters
 {
     /**
-     * @var Request
-     */
-    protected $request;
-    protected $builder;
-
-    /**
-     * ThreadFilters constructor.
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    /**
-     * Apply filters to threads query.
-     *
-     * @param $builder
-     * @return mixed
-     */
-    public function apply($builder)
-    {
-        $this->builder = $builder;
-
-        // Check if filter by username.
-        if ($this->request->has('by')) {
-            $this->by($this->request->by);
-        }
-
-        return $this->builder;
-    }
-
-    /**
      * Filters threads by username.
      *
      * @param $username
