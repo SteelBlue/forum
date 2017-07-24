3707a9919152d77d26f91893169fb7fc145357f9
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 18:12:45 -0400

    insert into the favorites table when FavoritesController@store is called

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Reply;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FavoritesController extends Controller
 {
-    public function store()
+    /**
+     * @param Reply $reply
+     */
+    public function store(Reply $reply)
     {
-        
+        DB::table('favorites')->insert([
+            'user_id' => auth()->id(),
+            'favorited_id' => $reply->id,
+            'favorited_type' => get_class($reply)
+        ]);
     }
 }
