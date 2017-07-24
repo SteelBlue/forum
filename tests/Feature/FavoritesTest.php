96c6848689122b0787023ffafc576b0eeafe2975
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 18:02:11 -0400

    building out the an_authenticated_user_can_favorite_any_reply test, added sudo code

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -10,8 +10,10 @@ class FavoritesTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function it_()
+    public function an_authenticated_user_can_favorite_any_reply()
     {
-        
+        // If I post to a "favorite" endpoint.
+
+        // It should be recorded in the database.
     }
 }
