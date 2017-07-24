95c75ea74b26e1c62be0a85b32987cd6e710a434
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 17:39:44 -0400

    order the threads by replies_count in desc order, added PHPdoc blocks

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -28,4 +28,14 @@ class ThreadFilters extends Filters
         // Fetch the threads, by a user.
         return $this->builder->where('user_id', $user->id);
     }
+
+    /**
+     * Filter the query according to most popular threads.
+     *
+     * @return $this
+     */
+    protected function popular()
+    {
+        $this->builder->orderBy('replies_count', 'desc');
+    }
 }
