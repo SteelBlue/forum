977608c82e1d3f32577755893b69b9ada50c44c3
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 19:11:49 -0400

    moved favorites->count within button

--- resources/views/threads/partials/reply.blade.php
+++ resources/views/threads/partials/reply.blade.php
@@ -6,12 +6,12 @@
             </h5>
 
             <div>
-                {{ $reply->favorites()->count() }}
-                
                 <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                     {{ csrf_field() }}
 
-                    <button type="submit" class="btn btn-default">Favorite</button>
+                    <button type="submit" class="btn btn-default">
+                        {{ $reply->favorites()->count() }} | {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
                 </form>
             </div>
         </div>
