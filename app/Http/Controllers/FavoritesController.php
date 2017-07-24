070cca289505582b1fb329057ef004d42a943df8
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 18:22:33 -0400

    added middleware to the FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -9,6 +9,14 @@ use Illuminate\Support\Facades\DB;
 class FavoritesController extends Controller
 {
     /**
+     * FavoritesController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * @param Reply $reply
      */
     public function store(Reply $reply)
