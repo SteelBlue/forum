db85da7ed4e53abb1dcd82da2d0ebdcee3817380
Ryan Ebbers <ebbers.seo@gmail.com>
2017-07-24 20:37:55 -0400

    pulled up members of Reply to Favoritable

--- app/Reply.php
+++ app/Reply.php
@@ -31,44 +31,4 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
-
-    /**
-     * A reply can be favorited.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    /**
-     * Favorite the current reply.
-     *
-     * @return Model
-     */
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-
-        // Check if user has not favorited the reply.
-        if (!$this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-    /**
-     * Determine if the current reply has been favorited.
-     *
-     * @return boolean
-     */
-    public function isFavorited()
-    {
-        return !! $this->favorites->where('user_id', auth()->id())->count();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
 }
